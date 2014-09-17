6ce9bbcb8253389ef16dfdcf9fae80b0e506c3f2
Andrew Goodnough <agoodnough@primexinc.com>
2014-09-16 21:06:55 -0500

    Boot the box not the app

--- setup-app.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-pushd /vagrant
-/home/vagrant/opt/activator-1.2.10/activator run
-popd
-
-exit 0
--- setup-play.sh
+++ setup-play.sh
@@ -7,4 +7,13 @@ popd
 chown -R vagrant:vagrant /home/vagrant/opt/activator-1.2.10
 chmod -R 700 /home/vagrant/opt/activator-1.2.10
 
+cat << 'EOF' >> /home/vagrant/.bashrc
+export ACTIVATOR_HOME=$HOME/opt/activator-1.2.10
+if [ -d "$ACTIVATOR_HOME" ] ; then
+    PATH="$ACTIVATOR_HOME:$PATH"
+fi
+EOF
+
+. /home/vagrant/.bashrc
+
 exit 0
